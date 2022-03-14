acfc04d5f9298a3fccbdd9d1ca04a249d31f2694
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2022-03-14 17:19:47 +0100

    problemi nell'incremento dei likes

--- js/main.js
+++ js/main.js
@@ -121,7 +121,7 @@ posts.forEach((element) => {
                     </a>
                 </div>
                 <div class="likes__counter">
-                    Piace a <b id="like-counter-1" class="js-likes-counter">${element["likes"]}</b> persone
+                    Piace a <b id="like-counter-${element["id"]}" class="js-likes-counter">${element["likes"]}</b> persone
                 </div>
             </div>
         </div>
@@ -131,10 +131,12 @@ posts.forEach((element) => {
 carouselContainerElement.innerHTML += carousel;
 
 
-
-
+let likeAdd = 0;
 
 document.querySelector('.likes__cta').addEventListener("click", function () {
     document.querySelector(".js-like-button").classList.add("like-button--liked");
+    /* js-likes-counter */
 
-})
+
+
+});
